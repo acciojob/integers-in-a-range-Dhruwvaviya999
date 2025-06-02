610978c00182f37522ee64020da22253bda527b3
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-06-02 12:39:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 // write your code here
 
+function range(start, end) {
+	let arr = [];
+	for(let i=start, i<=end; i++){
+		arr.push(i);
+	};
+	return arr;
+}
+
 // do not change the code below.
 let start = parseInt(prompt("Enter Start of the Range."));
 let end = parseInt(prompt("Enter End Of the Range."));
